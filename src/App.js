2337feb071f6ea2aac4ef5e1a822a778a39b55d8
Fatemeh <fatemehmd@gmail.com>
2024-11-10 22:39:59 -0500

    change router

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 // src/App.js
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import SpaceTunnel from './components/SpaceTunnel';
 import BoidSimulation from './components/BoidSimulation';
 import SpacePiano from './components/SpacePiano';
