6d5c93ae3ee02a3fa5c5bf04d036066f67305ac8
Fatemeh <fatemehmd@gmail.com>
2024-11-10 19:26:28 -0500

    Updates

---
+++ precache-manifest.2a6f5bc739a2328d98b52ca9b417b207.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82e9cdefe01b343e2358d6419eb3fbfa",
+    "url": "/bebop/index.html"
+  },
+  {
+    "revision": "e6172e60aa1af7d8f81b",
+    "url": "/bebop/static/css/main.b3194498.chunk.css"
+  },
+  {
+    "revision": "1582e325d1bcf091e744",
+    "url": "/bebop/static/js/2.ca150098.chunk.js"
+  },
+  {
+    "revision": "e6172e60aa1af7d8f81b",
+    "url": "/bebop/static/js/main.a1b8647d.chunk.js"
+  },
+  {
+    "revision": "233613bb5d599e48ed74",
+    "url": "/bebop/static/js/runtime~main.7f5f51de.js"
+  }
+]);
